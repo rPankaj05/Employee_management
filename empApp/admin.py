c3c5b757b403085eb23869067294ef80048bd97e
Pankaj Kumar <108936363+rPankaj05@users.noreply.github.com>
2023-09-23 17:01:20 +0530

    Update admin.py

--- empApp/admin.py
+++ empApp/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from empApp.models import Emp
+from empApp.models import Emp,Feedback
 
 class EmpAdmin(admin.ModelAdmin):
-    list_display=('name','working','emp_id','phone')
+    list_display=('name','working','emp_id','phone','department')
     list_editable=('working','emp_id')
     search_fields=('emp_id','name','phone' )
     list_filter=('working',)
 admin.site.register(Emp,EmpAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display=('name','email','phone','desc','date')
+admin.site.register(Feedback,FeedbackAdmin)
+
